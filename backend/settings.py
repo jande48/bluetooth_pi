d1940ce06fd651fc42dd69c1d7b672c312022c1d
Jacob Anderson <jacob@thepaperlessagent.com>
2023-11-09 16:59:56 -0600

    added localhost to allowed hosts

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure--0t5jb2(liy^8kw5w_eh49dh=!k2$l!o+g+514k8fm*n&r=oj=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0"]
+ALLOWED_HOSTS = ["0.0.0.0", "localhost"]
 
 
 # Application definition
